5a994800a00664e2c30510c4f84ce61e3717e2d4
michelm <michel.mooij7@gmail.com>
2013-09-27 18:17:05 +0200

    Refactoring makefile export to a stand-alone module.

--- waftools/wafexport/makefile.py
+++ waftools/wafexport/makefile.py
@@ -1,57 +1,87 @@
 #! /usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-'''This module contains an export formatter for converting C/C++ tasks
-into makefiles.
-'''
+"""
+Tool Description
+================
+This waftool can be used for the export and conversion of C/C++ task into
+Makefiles. When exporting all C/C++ tasks will be exported into one single
+makefile using all the compiler and linker directives as defined within the
+waf build environment. All tasks will be build and installed into the same
+location as they would when using the waf build environment. 
+
+The generated makefile supports following arguments:
+	'make all'
+	'make clean'
+	'make install'
+	'make uninstall'
+	'make <target>'
+
+Note that the makefile will use the PREFIX variable allowing users to specify
+a different install location, for example:
+	'make install PREFIX=/home/john'
+
+Usage
+=====
+In order for this waftool to work a special export function must be added to
+each single wscript within your waf build environment as presented in the 
+example below:
+
+	.
+	├── hello
+	│   ├── hello.c
+	│   └── wscript	-->	│#!/usr/bin/env python
+	│					│
+	│					│def build(bld):
+	│					│	bld.program(target='hello', source='hello.c')
+	│					└────────────────────────────────────────────────
+	│
+	├── bar.c
+	├── wscript		-->	│#!/usr/bin/env python
+	│					│
+	│					│APPNAME='hello'
+	│					│
+	│					│def options(opt):
+	│					│	opt.load('makefile')
+	│					│
+	│					│def configure(conf):
+	│					│	conf.load('makefile')
+	│					│
+	│					│def build(bld):
+	│					│	bld.program(target='foo', source='bar.c')
+	│					│	bld.recurse('hello')
+	│					└────────────────────────────────────────────────
+	│
+	├── Makefile		<-- makefile for the default environment
+	└── hello-win32.mk	<-- makefile for the variant named 'win32'
+
+"""
 
 import os, datetime
-from waflib import Logs, Node, Context
+from waflib import Build, Logs, Scripting, Task, Node, Context
 
-def init(bld):
-	'''initalizes the makefile export formatter'''
-	if not _selected(bld):
-		return
-	bld.makefile_commands = []
-	bld.makefile_targets = []
+def options(opt): pass
+def configure(conf): pass
 
-
-def process(task):
+def makefile_process(task):
 	'''(pre)processes and prepares the commands being executed per task into 
 	makefile targets and makefile commands.
 	'''
-	bld = task.generator.bld
-	if not _selected(bld):
-		return
 	name = task.__class__.__name__
 	if name in ('c','cxx'):
-		_compile_task(task)
+		makefile_compile(task)
 	elif name in ('cprogram', 'cshlib', 'cstlib', 'cxxprogram', 'cxxshlib', 'cxxstlib'):
-		_link_task(task)
+		makefile_link(task)
 
 
-def postfun(bld):
-	'''combines all preprocessed makefile targets and commands of all tasks
-	into a single makefile.
-	'''
-	if not _selected(bld):
-		return
-	_export_makefile(bld)
-
-
-def _selected(bld):
-	'''returns True when this export formatter has been selected.'''
-	return (set(bld.export_to) & set(['all','makefile']))
-
-
-def _compile_task(task):
+def makefile_compile(task):
 	'''converts a compile task into a makefile target.'''
 	bld = task.generator.bld
 	top = bld.bldnode.path_from(bld.path)
 	
 	# create a list of makefile targets
 	lst = ["%s/%s" % (top, o.relpath()) for o in task.outputs]
-	bld.makefile_targets.extend(lst)
+	bld.targets.extend(lst)
 
 	# convert command_executed into a makefile command	
 	try:
@@ -68,23 +98,23 @@ def _compile_task(task):
 					c = "%s/%s" % (top, c)
 				cmd.append(c)
 			task.command_executed = ' \\\n\t'.join(cmd)
-	except Exception as e:
-		bld.export_exception = (__file__, e, task)
+	except Exception as exception:
+		bld.export_exception = (exception, task, task.command_executed)
 	else:
 		target = lst.pop(0)
-		bld.makefile_commands.append('%s:' % target)
-		bld.makefile_commands.append('\tmkdir -p %s' % os.path.dirname(target))
-		bld.makefile_commands.append('\t%s' % task.command_executed)
+		bld.commands.append('%s:' % target)
+		bld.commands.append('\tmkdir -p %s' % os.path.dirname(target))
+		bld.commands.append('\t%s' % task.command_executed)
 
 
-def _link_task(task):
+def makefile_link(task):
 	'''converts a link task into a makefile target.'''
 	bld = task.generator.bld
 	top = bld.bldnode.path_from(bld.path)
 
 	# create list of targets
 	lst = ["%s/%s" % (top, o.relpath()) for o in task.outputs]
-	bld.makefile_targets.extend(lst)
+	bld.targets.extend(lst)
 
 	# create list of dependencies
 	deps = task.inputs + task.dep_nodes + bld.node_deps.get(task.uid(), [])
@@ -112,49 +142,59 @@ def _link_task(task):
 				cmd.append(c)
 			task.command_executed = ' \\\n\t'.join(cmd)
 	except Exception as e:
-		bld.export_exception = (__file__, e, task)
+		bld.export_exception = (exception, task, task.command_executed)
 	else:
-		bld.makefile_commands.append('%s: \\' % os.path.basename(str(lst.pop(0))))
-		bld.makefile_commands.append('\t%s' % ' \\\n\t'.join([str(l) for l in lst]))
-		bld.makefile_commands.append('\t%s' % task.command_executed)
+		bld.commands.append('%s: \\' % os.path.basename(str(lst.pop(0))))
+		bld.commands.append('\t%s' % ' \\\n\t'.join([str(l) for l in lst]))
+		bld.commands.append('\t%s' % task.command_executed)
 
 
-def _export_makefile(bld):
-	targets = list(bld.makefile_targets)
-	if not len(targets):
-		return
+def makefile_show_failure(bld):
+	(err, tsk, cmd) = bld.failure
+	msg = "export failure:\n"
+	msg += " tsk='%s'\n" % (str(tsk).replace('\n',''))
+	msg += " err='%r'\n" % (err)
+	msg += " cmd='%s'\n" % ('\n     '.join(cmd))
+	bld.fatal(msg)
 
+
+def makefile_export(bld):
 	bindir = str(bld.env.BINDIR)
 	libdir = str(bld.env.LIBDIR)
 	lines = []
 
-	tgt = [t for t in targets if not t.endswith('.dll.a')]
+	# makefile 'all'
+	tgt = [t for t in bld.targets if not t.endswith('.dll.a')]
 	tgt = [t if t.endswith('.o') else os.path.basename(t) for t in tgt]
 	lines.append("all: \\")
 	lines.append("\t%s" % ' \\\n\t'.join(tgt))
 
+	# makefile 'clean'
 	lines.append("")
 	lines.append("clean:")
-	for tgt in targets:
+	for tgt in bld.targets:
 		lines.append("\trm -rf  %s" % tgt)
 
+	# makefile 'install'
 	lines.append("")
 	lines.append("install:")
 	lines.append("\tmkdir -p %s" % bindir)
 	lines.append("\tmkdir -p %s" % libdir)
-	for t in [t for t in targets if t.split('.')[-1] not in ('a','o','so')]:
+	for t in [t for t in bld.targets if t.split('.')[-1] not in ('a','o','so')]:
 		lines.append("\tcp %s  %s/%s" % (t, bindir, os.path.basename(t)))
-	for t in [t for t in targets if t.endswith('so')]:
+	for t in [t for t in bld.targets if t.endswith('so')]:
 		lines.append("\tcp %s  %s/%s" % (t, libdir, os.path.basename(t)))
 
+	# makefile 'uninstall'
 	lines.append("")
 	lines.append("uninstall:")
-	for t in [t for t in targets if t.split('.')[-1] not in ('a','o','so')]:
+	for t in [t for t in bld.targets if t.split('.')[-1] not in ('a','o','so')]:
 		lines.append("\trm -rf  %s/%s" % (bindir, os.path.basename(t)))
-	for t in [t for t in targets if t.endswith('so')]:
+	for t in [t for t in bld.targets if t.endswith('so')]:
 		lines.append("\trm -rf  %s/%s" % (libdir, os.path.basename(t)))
 
-	for cmd in bld.makefile_commands:
+	# makefile <task.name>
+	for cmd in bld.commands:
 		if not cmd.startswith('\t'):
 			lines.append("")
 		lines.append(cmd)
@@ -185,6 +225,59 @@ def _export_makefile(bld):
 	Logs.warn('exported: %s' % node.abspath())
 
 
-	
+class ExportContext(Build.BuildContext):
+	'''exports and converts C/C++ tasks to MakeFile(s).'''
+	fun = 'build'
+	cmd = 'makefile'
 
+	def execute(self, *k, **kw):
+		self.failure = None
+		self.commands = []
+		self.targets = []
+
+		# install special task.exec_command that will store the actual
+		# command that has been executed (self.command_executed) as well 
+		# as the working directory (self.path)
+		old_exec = Task.TaskBase.exec_command
+		def exec_command(self, *k, **kw):
+			ret = old_exec(self, *k, **kw)
+			try:
+				cmd = k[0]
+			except IndexError:
+				cmd = ''
+			finally:
+				self.command_executed = cmd
+			try:
+				cwd = kw['cwd']
+			except KeyError:
+				cwd = self.generator.bld.cwd
+			finally:
+				self.path = cwd
+			return ret
+		Task.TaskBase.exec_command = exec_command
+
+		# install special task.process that will process/convert the stored
+		# executed command srting and working directory
+		old_process = Task.TaskBase.process
+		def process(self):
+			old_process(self)
+			makefile_process(self)
+		Task.TaskBase.process = process
+
+		# install post process that will export the processed and converted
+		# task data into the requested export format.
+		def postfun(self):
+			if self.failure:
+				makefile_show_failure(self)
+			elif not len(self.targets):
+				Logs.warn('makefile export failed: no suitable C/C++ targets found')
+			else:
+				makefile_export(self)
+		super(ExportContext, self).add_post_fun(postfun)
+
+		# remove results form previous build (if any)
+		Scripting.run_command('clean')
+
+		# start export
+		super(ExportContext, self).execute(*k, **kw)
 
